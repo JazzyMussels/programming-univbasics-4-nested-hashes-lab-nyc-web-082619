86edd2e2decc273be4b00f695448471211015ec4
Jeffrey Musselman <jeffmussels@gmail.com>
2019-08-18 05:51:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].value
+     programmer_hash[:dennis_ritchie][:languages].values 
 end
 
 def adding_matz
